507c806ab58ce63367f98477ddf4cc62e0ea5f76
evgen <evgenijfucenko2@gmail.com>
2023-12-14 17:07:48 +0200

    homework5

---
+++ homewrok5.py
@@ -0,0 +1,22 @@
+import requests
+import csv
+import time
+
+iss_api_url = "http://api.open-notify.org/iss-now.json"
+
+csv_filename = "iss_location_data.csv"
+
+with open(csv_filename, mode='w', newline='') as csv_file:
+    fieldnames = ['timestamp', 'latitude', 'longitude']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    while True:
+        response = requests.get(iss_api_url)
+        data = response.json()
+        timestamp = time.time()
+        latitude = data['iss_position']['latitude']
+        longitude = data['iss_position']['longitude']
+        writer.writerow({'timestamp': timestamp, 'latitude': latitude, 'longitude': longitude})
+        time.sleep(5)
+
